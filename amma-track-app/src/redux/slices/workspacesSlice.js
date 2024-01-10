b98945ffaa9acd1b46f9e5b6ed0ad43f09ec7be7
Artyom Harutyunyan <ka6rias@gmail.com>
2024-01-10 21:50:44 +0400

    Fix something

--- amma-track-app/src/redux/slices/workspacesSlice.js
+++ amma-track-app/src/redux/slices/workspacesSlice.js
@@ -12,7 +12,7 @@ export const workSpacesSlice = createSlice({
   initialState,
   reducers: {
     addWorkspace: (state, action) => {
-      state.workspaces.push({ id: new Date().toISOString(), title: action.payload.title, img: action.payload.img, user: action.payload, active: false })
+      state.workspaces.push({ id: new Date().toISOString(), title: action.payload.title, img: action.payload.img, user: action.payload.user, active: false })
       console.log(current(state).workspaces)
     },
     creationBoxHandle: (state, action) => {
