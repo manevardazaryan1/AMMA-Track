4147c86c3e9b27cb5e90108da929489b108d71eb
Mnae Vardazaryan <manevardazaryan@gmail.com>
2024-01-24 04:28:53 -0800

    Improve functionality of the card modal description section.

--- amma-track-app/src/redux/slices/cardModalSlice.js
+++ amma-track-app/src/redux/slices/cardModalSlice.js
@@ -11,16 +11,20 @@ export const cardModalSlice = createSlice({
     addDescription: (state, description) => {
       const desc = state.descriptions.find(card => card.cardID === description.payload.cardID);
 
-      if (!desc)
+      if (desc && description.payload.description === '<p><br></p>') {
+        state.descriptions = state.descriptions.filter(card => card.cardID !== description.payload.cardID);
+        return;
+      }
+
+      if (!desc) {
         state.descriptions = [...state.descriptions,  description.payload];
-      else {
+      } else {
         desc.description = description.payload.description
       }
       
     },
   },
 })
-
 
 export const { addDescription } = cardModalSlice.actions
 
