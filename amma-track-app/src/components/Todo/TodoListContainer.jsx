788ecb5f324eb78e7f6029161980fc303aa2598d
manevardazaryan1 <114493766+manevardazaryan1@users.noreply.github.com>
2024-02-13 06:42:24 -0800

    Update TodoListContainer.jsx

--- amma-track-app/src/components/Todo/TodoListContainer.jsx
+++ amma-track-app/src/components/Todo/TodoListContainer.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import TodoListCard from "./TodoListCards";
+import React from 'react';
+import TodoListCard from './TodoListCards';
 
 const TodoListContainer = ({
     lists,
@@ -36,35 +36,3 @@ const TodoListContainer = ({
 
 export default TodoListContainer
 
-  lists,
-  cards,
-  activeListId,
-  handleSetActiveList,
-  handleRemoveList,
-  handleRemoveCard,
-  handleAddCard,
-  newCardText,
-  setNewCardText,
-  boardId
-}) => {
-  return (
-    <div className="containerList">
-      {lists.map((list) => (
-        <TodoListCard
-          key={list.id}
-          list={list}
-          activeListId={activeListId}
-          handleSetActiveList={handleSetActiveList}
-          handleRemoveList={handleRemoveList}
-          handleRemoveCard={handleRemoveCard}
-          handleAddCard={handleAddCard}
-          newCardText={newCardText}
-          setNewCardText={setNewCardText}
-          cards={cards[list.id]}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default TodoListContainer;
