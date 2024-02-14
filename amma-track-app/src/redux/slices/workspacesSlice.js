7d533bd1eb998bb3738c097a1158f43ff7394040
Artur Abgaryan <arty219603@gmail.com>
2024-02-14 19:19:14 +0400

    Implemented account page with ability to change user image, password, username and ability deleting account

--- amma-track-app/src/redux/slices/workspacesSlice.js
+++ amma-track-app/src/redux/slices/workspacesSlice.js
@@ -1,20 +1,20 @@
-import { createSlice, current } from '@reduxjs/toolkit';
+import { createSlice, current } from "@reduxjs/toolkit";
 const initialState = {
   workspaces: [],
   selectedImg: {
-    thumb: '',
-  },
-}
+    thumb: ""
+  }
+};
 
 export const workSpacesSlice = createSlice({
-  name: 'workspaces',
+  name: "workspaces",
   initialState,
   reducers: {
     addWorkspace: (state, action) => {
       //state.workspaces = state.workspaces.map(workspace => { return { ...workspace, active: false } })
       //state.workspaces.push({ id: action.payload.id, title: action.payload.title, img: action.payload.img, user: action.payload.user, active: action.payload.active })
       //console.log(current(state).workspaces)
-      state.workspaces = [action.payload, ...state.workspaces]
+      state.workspaces = [action.payload, ...state.workspaces];
     },
     workspaceCreationBoxHandle: (state, action) => {
       state.creationBox = action.payload.val;
@@ -23,20 +23,29 @@ export const workSpacesSlice = createSlice({
       state.selectedImg.thumb = action.payload.thumb;
     },
     toggleActiveWorkspace: (state, action) => {
-      state.workspaces = state.workspaces.map(workspace => {
+      state.workspaces = state.workspaces.map((workspace) => {
         if (workspace.id === action.payload.id) {
-          return { ...workspace, active: true }
-        }
-        else
-          return { ...workspace, active: false }
-      })
-
+          return { ...workspace, active: true };
+        } else return { ...workspace, active: false };
+      });
+    },
+    clearUserWorkspaces: (state, action) => {
+      state.workspaces = state.workspaces.filter(
+        (workspace) => workspace.user.id !== action.payload.userId
+      );
     },
     deleteActiveWorkspace: (state, action) => {
-      state.workspaces = state.workspaces.map(workspace => { return { ...workspace, active: false } })
+      state.workspaces = state.workspaces.map((workspace) => {
+        return { ...workspace, active: false };
+      });
     }
-
   }
-})
-export const { addWorkspace, selectWorkspaceImg, toggleActiveWorkspace, deleteActiveWorkspace } = workSpacesSlice.actions
-export default workSpacesSlice.reducer
+});
+export const {
+  addWorkspace,
+  selectWorkspaceImg,
+  toggleActiveWorkspace,
+  clearUserWorkspaces,
+  deleteActiveWorkspace
+} = workSpacesSlice.actions;
+export default workSpacesSlice.reducer;
