e0249812bf832d6341e430e07a1f6867cd10880a
kazrias <ka6rias@gmail.com>
2024-02-15 23:15:29 +0400

    Fix css

--- amma-track-app/src/components/Todo/TodoListContainer.jsx
+++ amma-track-app/src/components/Todo/TodoListContainer.jsx
@@ -1,8 +1,8 @@
 import TodoListCard from './TodoListCards';
 
 import { useDispatch } from 'react-redux';
-import {updateLists} from "../../redux/slices/todosSlice.js"
-import {updateCardOrder} from "../../redux/slices/cardsSlice.js"
+import { updateLists } from "../../redux/slices/todosSlice.js"
+import { updateCardOrder } from "../../redux/slices/cardsSlice.js"
 
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
@@ -20,10 +20,10 @@ const TodoListContainer = ({
   boardId,
 }) => {
   const dispatch = useDispatch();
- 
+
   const handleDragEnd = (result) => {
     if (!result.destination) return;
-  
+
     if (result.type === 'LIST') {
       const updatedLists = Array.from(lists);
       const [removed] = updatedLists.splice(result.source.index, 1);
@@ -32,7 +32,7 @@ const TodoListContainer = ({
     } else if (result.type === 'CARD') {
       const sourceListId = result.source.droppableId;
       const destinationListId = result.destination.droppableId;
-  
+
       if (sourceListId === destinationListId) {
         const sourceList = Array.isArray(cards[sourceListId]) ? [...cards[sourceListId]] : [];
         const [removed] = sourceList.splice(result.source.index, 1);
@@ -48,38 +48,42 @@ const TodoListContainer = ({
       }
     }
   };
-  
+
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="todo-lists" direction="horizontal" type="LIST">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps} className="containerList">
-            {lists.map((list, index) => (
-              <Draggable key={list.id} draggableId={list.id} index={index} type="LIST">
-                {(provided) => (
-                  <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                    <TodoListCard
-                      key={list.id}
-                      list={list}
-                      activeListId={activeListId}
-                      handleSetActiveList={handleSetActiveList}
-                      handleRemoveList={handleRemoveList}
-                      handleRemoveCard={handleRemoveCard}
-                      handleAddCard={handleAddCard}
-                      newCardText={newCardText}
-                      setNewCardText={setNewCardText}
-                      cards={cards[list.id]}
-                      index={index}
-                    />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <>
+      {
+        !lists.length ? <p style={{display:"flex",justifyContent:"center",alignItems:"center",minHeight:"300px",fontSize:"48px"}} className="containerList">Add your first list!</p> : <DragDropContext onDragEnd={handleDragEnd}>
+          <Droppable droppableId="todo-lists" direction="horizontal" type="LIST">
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps} className="containerList">
+                {lists.map((list, index) => (
+                  <Draggable key={list.id} draggableId={list.id} index={index} type="LIST">
+                    {(provided) => (
+                      <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <TodoListCard
+                          key={list.id}
+                          list={list}
+                          activeListId={activeListId}
+                          handleSetActiveList={handleSetActiveList}
+                          handleRemoveList={handleRemoveList}
+                          handleRemoveCard={handleRemoveCard}
+                          handleAddCard={handleAddCard}
+                          newCardText={newCardText}
+                          setNewCardText={setNewCardText}
+                          cards={cards[list.id]}
+                          index={index}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      }
+    </>
   );
 };
 
