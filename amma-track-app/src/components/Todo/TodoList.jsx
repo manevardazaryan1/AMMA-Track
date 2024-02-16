ab4c45578dd7317a5fa985eae71524d3e1e137ae
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2024-02-16 22:11:09 +0400

    add submit in add list and add card

--- amma-track-app/src/components/Todo/TodoList.jsx
+++ amma-track-app/src/components/Todo/TodoList.jsx
@@ -1,29 +1,21 @@
 import './TodoList.css';
 import { useState, useEffect } from 'react';
-
 import TodoListContainer from './TodoListContainer';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { addList, removeList } from '../../redux/slices/todosSlice';
 import { addCard, removeCard } from '../../redux/slices/cardsSlice';
-
 import { db } from '../../config/firebaseConfig';
 import { collection, addDoc, getDocs, deleteDoc, doc } from 'firebase/firestore';
-
-
 const TodoList = ({ boardId }) => {
   const dispatch = useDispatch();
   const lists = useSelector(state => state.todos);
   const cards = useSelector(state => state.cards);
-
   const [newListTitle, setNewListTitle] = useState('');
   const [newCardText, setNewCardText] = useState('');
   const [activeListId, setActiveListId] = useState(null);
   const [showForm, setShowForm] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
   const currentTodos = lists.filter(todo => todo.boardId === boardId)
-
   useEffect(() => {
     const fetchLists = async () => {
       setIsLoading(true)
@@ -32,10 +24,8 @@ const TodoList = ({ boardId }) => {
       snapshot.docs.reverse().map((doc) => (dispatch(addList({ ...doc.data() }))))
       setIsLoading(false)
     }
-
     if (!lists.length) fetchLists()
   }, [lists.length, dispatch])
-
   useEffect(() => {
     const fetchCards = async () => {
       const cardsCollection = collection(db, 'cards')
@@ -44,14 +34,12 @@ const TodoList = ({ boardId }) => {
     }
     if (!Object.keys(cards).length) fetchCards()
   }, [cards, Object.keys(cards).length, dispatch])
-
   const generateUniqueId = () => {
     return new Date().getTime().toString();
   };
-
   const handleToggleForm = () => setShowForm(!showForm);
-
-  const handleAddList = async () => {
+  const handleAddList = async (e) => {
+    e.preventDefault();
     if (newListTitle.trim() !== '') {
       const newListId = generateUniqueId();
       const listsCollection = collection(db, 'lists');
@@ -67,7 +55,6 @@ const TodoList = ({ boardId }) => {
       setNewListTitle("");
     }
   };
-
   const handleRemoveList = async (listId) => {
     if (activeListId === listId) {
       setNewCardText('');
@@ -76,15 +63,14 @@ const TodoList = ({ boardId }) => {
     dispatch(removeCard({ listId }));
     const listsCollection = collection(db, "lists")
     const snapshot = await getDocs(listsCollection)
-
     for (let list of snapshot.docs.filter(doc => doc.data().id === listId)) {
       if (list.id)
         await deleteDoc(doc(db, 'lists', list.id))
     }
     dispatch(removeList(listId));
   };
-
-  const handleAddCard = async () => {
+  const handleAddCard = async (e) => {
+    e.preventDefault();
     if (newCardText.trim() !== '' && activeListId !== null) {
       const newCardId = generateUniqueId();
       const cardsCollection = collection(db, 'cards');
@@ -101,18 +87,15 @@ const TodoList = ({ boardId }) => {
       setNewCardText('');
     }
   };
-
   const handleRemoveCard = async (listId, cardId) => {
     dispatch(removeCard({ listId, cardId }));
     const cardsCollection = collection(db, "cards")
     const snapshot = await getDocs(cardsCollection)
-
     for (let card of snapshot.docs.filter(doc => doc.data().card.id === cardId)) {
       if (card.id)
         await deleteDoc(doc(db, 'cards', card.id))
     }
   };
-
   const handleSetActiveList = (listId) => {
     setActiveListId(listId);
   };
@@ -120,15 +103,17 @@ const TodoList = ({ boardId }) => {
     <div className='to-do-lists'>
       {
         showForm ? (
-          <div className='add-list-form list-buttons'>
-            <input
-              type="text"
-              placeholder="Add another list"
-              value={newListTitle}
-              onChange={e => setNewListTitle(e.target.value)}
-            />
-            <button onClick={handleAddList}><i className="fa-solid fa-check"></i></button>
-            <button onClick={handleToggleForm}><span><i className="fa-solid fa-x"></i></span></button>
+          <div className='add-list-form'>
+            <form onSubmit={handleAddList} className='list-buttons'>
+              <input
+                type="textn"
+                placeholder="Add another list"
+                value={newListTitle}
+                onChange={(e) => setNewListTitle(e.target.value)}
+              />
+              <button type="submit"><i className="fa-solid fa-check"></i></button>
+              <button onClick={handleToggleForm}><i className="fa-solid fa-x"></i></button>
+            </form>
           </div>
         ) : (
           <button className="add-list list-buttons" onClick={handleToggleForm}>
@@ -151,5 +136,4 @@ const TodoList = ({ boardId }) => {
     </div>
   );
 };
-
 export default TodoList;
